104e36f117ac3d7cc3ed7c86b6ac2849638e2782
Magistra Apta <31767116+magistraapta@users.noreply.github.com>
2025-04-01 13:49:37 +0800

    feat: add cors middleware

---
+++ middleware/cors.go
@@ -0,0 +1,22 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*") // Allow all origins (change if needed)
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent) // 204 response for preflight
+			return
+		}
+
+		c.Next()
+	}
+}
