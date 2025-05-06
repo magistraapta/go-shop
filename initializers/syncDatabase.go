980eee37a8a44af54937330eb9fd3f57583cf7e4
Magistra Apta <31767116+magistraapta@users.noreply.github.com>
2025-05-06 13:08:19 +0800

    fix: remove

--- initializers/syncDatabase.go
+++ initializers/syncDatabase.go
@@ -1,19 +1,1 @@
 package initializers
-
-import (
-	"golang-shop/internal/model"
-	"log"
-)
-
-func SyncDatabase() {
-	db, err := ConnectDatabase()
-	db.AutoMigrate(
-		&model.User{}, &model.Product{},
-		&model.Cart{}, &model.CartItem{},
-		&model.Transaction{}, &model.OrderItem{},
-		&model.Payment{})
-
-	if err != nil {
-		log.Fatalf("Failed to migrate database: %v", err)
-	}
-}
